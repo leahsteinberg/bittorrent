0a9b63b7bafca8e63fd86824acd362497176d6ca
leahsteinberg <leahsteinberg1@gmail.com>
2014-03-12 15:15:32 -0400

    made more classes. the piece class

--- client.py
+++ client.py
@@ -4,14 +4,6 @@ from messages import *
 from torrent import *
 import time
 
-def main_func():
-  article = bencode.bdecode(open('demo.torrent', 'rb').read())
-  torr = Torrent(article['info']['name'])
-  parameters = open_torrent(torr, article)
-  tracker = Tracker(get_peers(parameters))
-  for peer in tracker.peers:
-    if handshake(peer): 
-      send_request(peer)
 
 def open_torrent(torr, article):
   torr.tracker_url = article['announce']
@@ -28,7 +20,6 @@ def open_torrent(torr, article):
           torr.piece_length = article['info']['files'][0]['piece length']
 	  for i in range(len(article['info']['files'])):
 	  	torr.length += article['info']['files'][i]['length']
-               
   parameters = {'info_hash': info_hash, 'peer_id': peer_id,
 	'port': port, 'uploaded': 0, 'downloaded': 0, 'left': torr.length, 'compact': 1, 'event': 'started'}
   return parameters
@@ -50,14 +41,15 @@ def get_peers(parameters):
 
 
 ## if the handshake works, this should clear out the received buffer once it returns. (is this true??)
-def handshake(peer):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect((tracker.peers[i].ip_addr, tracker.peers[i].port))
+## ISSUE ... HOW DOES THIS GET INFO OF INFO HASH AND PEER ID!!!!!!!!
+def handshake(peer, torr):
+  peer.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  peer.socket.connect((peer.ip_addr, peer.port))
   pst = chr(19)
   reserved = chr(0)*8
   handshake = pst+'BitTorrent protocol'+reserved+torr.info_hash+torr.peer_id
-  sock.send(handshake)
-  recv_buff = sock.recv(100)
+  peer.socket.send(handshake)
+  recv_buff = peer.socket.recv(100)
   if recv_buff[28:48] == torr.info_hash:
     recv_buff = recv_buff[68:]
   else:
@@ -67,25 +59,38 @@ def handshake(peer):
   return True
 
 
-def get_messages():
-    recv_buff = sock.recv(100)
-    while len(recv_buff)>0:
-      recv_buff = parse(recv_buff, peers[i])
-    # send interested
+def send_interested(peer):
     interested = "\x00\x00\x00\x01\x02"
-    sock.send(interested)
-    recv_buff = sock.recv(100)
+    peer.socket.send(interested)
+    recv_buff = peer.socket.recv(100)
     print repr(recv_buff)
     while len(recv_buff)>0:
-      recv_buff = parse(recv_buff, peers[i])
-    pieces_set = set(peers[i].pieces)
-    print pieces_set
-    for piece in pieces_set:
-      hex_piece = chr(piece)
+      recv_buff = peer.parse(recv_buff)
+
+def request_pieces(peer):
+    for piece in set(peers.pieces):
       request = "\x00\x00\x00\x0d\x06" + "\x00\x00\x00" + chr(piece) + '\x00\x00\x00\x00' + int_to_bytes(2**14)
-      sock.send(request)
+      peer.socket.send(request)
       time.sleep(.1)
-      data = sock.recv(2**14+13)
-      print str(piece) + "  got piece: " + str(len(data))
-      parse(data, peers[i])
+      data = peer.socket.recv(2**14+13)
+      peer.parse(data)
+
+
+def main_func():
+  article = bencode.bdecode(open('demo.torrent', 'rb').read())
+  torr = Torrent(article['info']['name'])
+  parameters = open_torrent(torr, article)
+  tracker = Tracker(get_peers(parameters))
+  for peer in tracker.peers:
+    if handshake(peer, torr): 
+      send_interested(peer)
+  for peer in tracker.peers:
+    request_pieces(peer)
+
+
+if __name__ == '__main__':
+    main_func()
+
+
+
 
--- torrent.py
+++ torrent.py
@@ -1,11 +1,11 @@
 class Torrent:
   def __init__(self, name):
-    self.name = None #
-    self.tracker_url = None #
-    self.info_hash = None #
-    self.length = None #
-    self.piece_length = None #
-    self.file_d = open(name, 'w') #
+    self.name = None 
+    self.tracker_url = None 
+    self.info_hash = None 
+    self.length = None 
+    self.piece_length = None 
+    self.file_d = open(name, 'w') 
     
     
 
@@ -15,25 +15,23 @@ class Tracker:
 
 
 class Peer:
-  def __init__(ip_addr, peer_port):
+  def __init__(self, ip_addr, peer_port):
     self.ip_addr = ip_addr
+    self.socket = None
     self.port = port
     self.pieces = []
     self.choked = True
     self.messages = []
     message_ids = [choke, unchoke, interested, not_interested, have, bitfield, request, piece, cancel, port]
 
-
-  
   def choke(message):
     #print "choke"
-    peer.choked = True
+    self.choked = True
 
   def unchoke(message):
     #print "unchoke"
-    peer.choked = False
+    self.choked = False
   
-
   def interested(message):
     pass
 
@@ -44,7 +42,7 @@ class Peer:
     #print "in have: "
     piece_have = ord(message[0])*(256**3)+ord(message[1])*(256**2)+ord(message[2])*(256**1)+ord(message[3])
     #passes in 4 byte payload -> representing one number.
-    peer.pieces.append(piece_have)
+    self.pieces.append(piece_have)
     #print peer.pieces
 
   def request(message):
@@ -54,13 +52,22 @@ class Peer:
     # each four bytes... (same as request)
     # which piece we're talking about
     # where in the piece we are
-    # length of piece
-    # here is where I want to write to file object
-    ## also get rid of other stuff
-    #file.write message from 12 on
+    # 
+
+    ## what I want to do here is:::
+    # i have a message
+    # i need to figure out what piece it is in
+    # and find that piece object
+    # then I want to make a temporary file
+    # then I want to make a new block entry in the piece object
+    # this block entry will be a tuple
+    # the tuple will be (offset, temp_file)
+    # i will get the offset from a place in the message
+
+    #peer.piece[number]
     message = message[8:]
-    peer.f.write(message)
-    peer.f.flush()
+    self.file_d.write(message)
+    self.file_d.flush()
 
   def cancel(message):
     pass
@@ -71,33 +78,30 @@ class Peer:
   def bitfield(message):
     #print "in bitfield" + repr(message) + "\n"
     bit_field = ''
-    #print len(message)
-    #print repr(message)
-    for i in range(len(message)):  ## deal w endian issue
-      #print "i in bit: " + str(i)
+    for i in range(len(message)):
       bit_field += str(bin(ord(message[i]))[2:])
     #print bit_field
     for j in range(len(bit_field)):
       if bit_field[j]=='1':
-        peer.pieces.append(j)
-    #print "end of bitfield: "
-    #print peer.pieces
-    
+        self.pieces.append(j)
 
-  def parse(buff, peer):
+  def parse(buff):
     #print "start of parse"
     message_len = ord(buff[0])*(256**3)+ord(buff[1])*(256**2)+ord(buff[2])*(256**1)+ord(buff[3])
-    id = ord(buff[4])
+    message_type = ord(buff[4])
     # grab the message from the string based on the length
     message = buff[5:message_len+5]
-    #print "in parse: " + repr(message)
-    message_ids[id](message, peer)
+    self.message_ids[message_type](message)
     buff = buff[message_len+4:]
     return buff
+
+  def write_file():
+
 
 
 
 
 class Piece:
-  def __init__():
-    pass
+  def __init__(self, number):
+    self.number = number
+    self.blocks = []
